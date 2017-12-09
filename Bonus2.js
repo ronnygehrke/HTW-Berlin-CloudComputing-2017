0c42b0a8fb8b16f6056c0a5d9981999ba47e28ba
Ronny Gehrke <ronny.gehrke@aperto.com>
2017-12-09 01:46:01 -0800

    add code

--- Bonus2.js
+++ Bonus2.js
@@ -10,49 +10,25 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
     var queryData = url.parse(request.url, true).query;
-    var firstname =  queryData.fn;
-    var lastname =  queryData.ln;
     var requestId = crypto.randomBytes(16).toString("hex");
 
     var headers = request.headers;
     var authorization = headers['authorization'];
    
-    var username = null;
-    var password = null;
-
-    if (authorization)
-    {
-        var tmp = authorization.split(' ');     // Split on a space, the original auth looks like  "Basic Y2hhcmxlczoxMjM0NQ==" and we need the 2nd part
-        var buf = new Buffer(tmp[1], 'base64'); // create a buffer and tell it the data coming in is base64
-        var plain_auth = buf.toString();        // read it back out as a string At this point plain_auth = "username:password"
-
-        var creds = plain_auth.split(':');      // split on a ':'
-        username = creds[0];
-        password = creds[1];
-    }
-
-   if(request.method=='POST') 
+   if(request.method=='GET') 
    {
         var body = [];
         request.on('data', function(chunk) {
             body.push(chunk);
         }).on('end', function() {
             body = Buffer.concat(body).toString();
-                // at this point, `body` has the entire request body stored in it as a string
-                var responseMessage = 'Your provided e-mail address is: '+body;
-                
-                if (authorization) 
-                    responseMessage += '\nYour provided Authorization header is: '+authorization;
-                
-                if (username)
-                    responseMessage += '\nYour provided username is: '+username;
 
-                if (password)
-                    responseMessage += '\nYour provided password is: '+password;
-                
+				if (process.pid) {
+					responseMessage += 'This process is your pid: ' + process.pid;
+				}
+				
                 responseMessage += '\n\nRequest Id: '+requestId;
                 response.end(responseMessage);
-       
             });       
     }
     else
